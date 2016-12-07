b68a1d1a9ce8979d1b74c80038874c42e112845d
53by97 <srimant.sahu@gmail.com>
2016-12-08 02:22:25 +0530

    changed db script to store proper username

--- src/main/resources/dml_script.sql
+++ src/main/resources/dml_script.sql
@@ -1,2 +1,7 @@
 INSERT INTO restdata VALUES(123, 'Srimanta', 123.45, CURRENT_TIMESTAMP);
+
+UPDATE restdata SET username = 'Mr.Srimanta' WHERE username = 'Srimanta';
+
 COMMIT;
+
+SELECT * FROM restdata;
